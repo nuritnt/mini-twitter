cb7c23238de2458e6ee2a4e97e1c1c92ec21f1d6
Tugce Nur Tas <tas.tugcenur@gmail.com>
2022-05-31 12:27:24 +0200

    connect Message model to Controller and display in view with foreach

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -12,4 +12,19 @@
 
 <h2>Recent messages:</h2>
 
+<!-- loops through the $messages array -->
+<!-- this blade template gets the array from the MessageController.php -->
+<!-- for each element of the loop (which we call $message) -->
+<!-- we print the properties (titles, content, created_at) in an <li> element -->
+
+<ul>
+@foreach ($messages as $message)
+    <li>
+        <span>{{ $message->title }}</span><br>
+        <span>{{ $message->content }}</span><br>
+        <span>{{ $message->created_at->diffForHumans() }}</span><br>
+    </li>
+@endforeach
+</ul>
+
 @endsection
