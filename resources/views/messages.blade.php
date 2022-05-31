6e408d3f51f2c1bffcc0acf4839c3a0e28fb077e
Tugce Nur Tas <tas.tugcenur@gmail.com>
2022-05-31 11:44:13 +0200

    init

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,15 @@
+<!-- extent layout master.blade.php -->
+@extends('master')
+
+<!-- sets value for section title to "Mini Twitter" -->
+<!-- section title is used in messages.blade.php -->
+@section('title', 'Mini Twitterli')
+
+<!-- starts section content, defines some html for section and end content -->
+<!-- puts value for section title to "Mini Twitter"  -->
+<!-- section content is used in messages.blade.php -->
+@section('content')
+
+<h2>Recent messages:</h2>
+
+@endsection
